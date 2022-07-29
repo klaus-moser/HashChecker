525197a2ff88f4eee8d68f51ca1f84e45d6783cd
klaus-moser <klaus-moser@posteo.de>
2022-07-29 22:04:39 +0200

    Add test

---
+++ src/view.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QDialog, QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
+
+from view_orig import Ui_MainWindow
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+    def about(self):
+        QMessageBox.about(
+            self,
+            "About HashCheacker",
+            "<p>An simple app to compare two hash codes/codefiles:</p>"
+            "<p>- PyQt</p>"
+            "<p>- Qt Designer</p>"
+            "<p>- Python3</p>",
+        )
