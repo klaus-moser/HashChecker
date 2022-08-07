f79ab740ba042a93011866809875dfee418cdff1
klaus-moser <klaus-moser@posteo.de>
2022-08-07 18:02:01 +0200

    Add prints to view

--- src/view.py
+++ src/view.py
@@ -8,6 +8,7 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.radioButton_sha256.setChecked(True)
 
     def about(self) -> None:
         """
@@ -22,3 +23,12 @@ class Window(QMainWindow, Ui_MainWindow):
             "<p>- Qt Designer</p>"
             "<p>- Python3</p>",
         )
+
+    def print_text(self, text: str) -> None:
+        self.textEdit_output.setPlainText(text)
+
+    def clear_text(self) -> None:
+        self.textEdit_output.clear()
+
+    def get_text(self) -> list:
+        return [self.lineEdit_h1.text(), self.lineEdit_h2.text()]
